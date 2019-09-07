89ce2c90698771b7313f16a9139668b2e713c7f8
Francisco Vena <fvena@altracorporacion.es>
2019-09-07 12:27:27 +0200

    add assets to lib

---
+++ packages/assets/icons/sprite/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+require('./clear')
+require('./down')
+require('./info')
+require('./left')
+require('./password-hide')
+require('./password')
+require('./right')
+require('./up')
+require('./user-fill')
